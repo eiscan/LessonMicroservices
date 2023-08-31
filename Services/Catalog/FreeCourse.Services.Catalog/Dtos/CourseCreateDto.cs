715d00ff84eec758831f06fbfb8a72e770b60f06
EKREM İŞCAN <eiscan@outlook.com.tr>
2023-08-31 23:19:11 +0300

    ADD dtos for catalog services

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Services.Catalog.Models;
+using System;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseCreateDto
+    {
+        public string Name { get; set; }
+        public string Ddescription { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+        public Feature Feature { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
