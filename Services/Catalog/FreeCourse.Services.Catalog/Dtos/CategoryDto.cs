efa394cb1abb2caa8cb9cb4fe3cc4bd1d17daca7
EKREM İŞCAN <eiscan@outlook.com.tr>
2023-09-02 08:31:45 +0300

    ADD Category service

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson;
 
 namespace FreeCourse.Services.Catalog.Dtos
 {
-    public class CategoryDto
+    internal class CategoryDto
     {
         public string Id { get; set; }
         public string Name { get; set; }
